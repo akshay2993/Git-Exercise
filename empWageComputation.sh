35e1d1be664a84d4b4fec7b5cfec3d2547536ab3
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-04-15 00:34:54 +0530

    UC-1:Employee Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
 echo "Welcome to the Employee Wage Computation Program!"
+
+## Use Case-1 #### (check employee present or absent)
+
+isPresent=$((RANDOM%2));
+
+case $isPresent in
+        0)
+        	echo "Employee is absent";
+                ;;
+
+        1)
+                echo "Employee is present";
+             	;;
+esac
