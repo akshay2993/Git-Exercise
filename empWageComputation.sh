49be40168817df2e328b1936f03075625bc7cf3a
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-04-15 00:59:06 +0530

    UC-2:Daily employee wage calculation

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,17 +1,22 @@
-#!/bin/bash
+#!/bin/bash -x
 
 echo "Welcome to the Employee Wage Computation Program!"
 
-## Use Case-1 #### (check employee present or absent)
-
+## Use Case-2 #### (calculate daily employee wage)
+wagePerHour=20
+workingHours=0
 isPresent=$((RANDOM%2));
 
 case $isPresent in
         0)
-        	echo "Employee is absent";
+		dailyWage=$(( $wagePerHour*$workingHours ))
+        	echo "Employee is absent. So today's wage : $dailyWage";
                 ;;
 
         1)
-                echo "Employee is present";
+		workingHours=8
+                dailyWage=$(( $wagePerHour*$workingHours ))
+                echo "Employee is present. So today's wage : $dailyWage";
+
              	;;
 esac
