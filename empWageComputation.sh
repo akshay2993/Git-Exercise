6ff3775d361e108d343103d961146808b24a0456
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-04-15 18:46:31 +0530

    UC1: Employee present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #!/bin/bash -x
 
 echo "Welcome to the Employee Wage Computation Program!"
+
+isPresent=$((RANDOM%2));
+
+if [ $isPresent -eq 0 ]
+then
+        echo "Employee is absent";
+elif [ $isPresent -eq 1 ]
+then
+        echo "Employee is present";
+fi
