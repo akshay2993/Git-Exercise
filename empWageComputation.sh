1cc4f459049ed9d4f5cb6a9d7234df06e461d2b9
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-04-15 21:57:50 +0530

    Monthly wage calculation:UC-5

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,22 +5,28 @@ echo "Welcome to the Employee Wage Computation Program!"
 wagePerHour=20
 workingHours=0
 isPresent=$((RANDOM%3));
+totalSalary=0
 
-case $isPresent in
-	0)
-	echo "Employee is absent";
-	;;
+for ((day=1; day<=20; day++))
+do
+	case $isPresent in
+        	0)
+        	echo "Employee is absent";
+        	;;
 
-	1)
-	echo "Employee is present";
-	workingHours=8;
-	;;
+        	1)
+        	echo "Employee is present";
+        	workingHours=8;
+        	;;
 
-	2)
-	echo "Employee is working Part-time";
-	workingHours=4;
-	;;
-esac
+        	2)
+        	echo "Employee is working Part-time";
+        	workingHours=4;
+        	;;
+	esac
 
-dailyWage=$(( $wagePerHour*$workingHours ))
-echo "Employee's today's wage : $dailyWage $";
+	dailyWage=$(( $wagePerHour*$workingHours ))
+	totalSalary=$(( $dailyWage+$totalSalary ))
+done
+
+echo "Employee has earned $totalSalary $ this month.";
