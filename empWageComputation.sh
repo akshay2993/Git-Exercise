46e38e0cdf1fee88ef7ec3c196089411c5c7056d
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-04-16 00:32:41 +0530

    Added condition of max days/hours: UC-6

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -37,44 +37,4 @@ do
         ((day++));
 done
 
-
-
-
 echo "Employee has earned $totalSalary $ this month.";
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#for ((day=1; day<=20; day++))
-#do
-#	case $isPresent in
-#        	0)
-#        	echo "Employee is absent";
-#        	;;
-#
-#        	1)
-#        	echo "Employee is present";
-#        	workingHours=8;
-#        	;;
-#
-#        	2)
-#        	echo "Employee is working Part-time";
-#        	workingHours=4;
-#        	;;
-#	esac
-#
-#	dailyWage=$(( $wagePerHour*$workingHours ))
-#	totalSalary=$(( $dailyWage+$totalSalary ))
-#done
-#
-#echo "Employee has earned $totalSalary $ this month.";
