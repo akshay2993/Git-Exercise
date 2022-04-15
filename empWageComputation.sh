f4ef96845a2f53a17a448522cc669b41978b678f
akshayshaerma.2993@gmail.com <akshaysharma.2993@gmail.com>
2022-04-15 18:40:31 +0530

    corrected

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,22 +1,3 @@
 #!/bin/bash -x
 
 echo "Welcome to the Employee Wage Computation Program!"
-
-## Use Case-2 #### (calculate daily employee wage)
-wagePerHour=20
-workingHours=0
-isPresent=$((RANDOM%2));
-
-case $isPresent in
-        0)
-		dailyWage=$(( $wagePerHour*$workingHours ))
-        	echo "Employee is absent. So today's wage : $dailyWage";
-                ;;
-
-        1)
-		workingHours=8
-                dailyWage=$(( $wagePerHour*$workingHours ))
-                echo "Employee is present. So today's wage : $dailyWage";
-
-             	;;
-esac
